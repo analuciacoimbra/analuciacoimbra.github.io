aecf2d580f4613ff529520333f0b39ce3ba8822c
Ana Lúcia Coimbra <130006486+analuciacoimbra@users.noreply.github.com>
2025-03-26 19:55:44 +0000

    Update link-conversion.js

--- link-conversion.js
+++ link-conversion.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function ()
 {
-	const a = document.getElementById('link-conversion');
+	const a = document.getElementById('email');
 
 	a.setAttribute('href', a.getAttribute('href')
 		.replace('-', 'ptcoimbra@')
