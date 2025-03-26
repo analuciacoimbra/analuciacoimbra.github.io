9c027457ef6bebb527eead28c576d62b03f1379c
Ana Lúcia Coimbra <130006486+analuciacoimbra@users.noreply.github.com>
2025-03-26 21:33:33 +0000

    Update link-conversion.js

--- link-conversion.js
+++ link-conversion.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function ()
 
 	a.setAttribute('href', a.getAttribute('href')
 		.replace('-', 'oimbra@')
-        .replace('me', 'luciaptc')
+		.replace('me', 'luciaptc')
 		.replace('/', 'gmail.com')
 		.replace('to', 'mailto:ana')
 	);
