fc69070f44ed68c08039524e18400063f48ce293
Ana Lúcia Coimbra <130006486+analuciacoimbra@users.noreply.github.com>
2025-03-26 19:50:35 +0000

    Update e-mail

---
+++ link-conversion.js
@@ -0,0 +1,13 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+	const a = document.getElementById('email');
+
+	a.setAttribute('href', a.getAttribute('href')
+		.replace('-', 'ptcoimbra@')
+		.replaceAll('-', '.')
+		.replace('/', 'gmail.com')
+		.replace('to', 'mailto:a')
+	);
+});
